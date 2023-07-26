c281dbb0d28e9e23dae57dc716fc13fe936785f8
harshkumar0077 <harsh.kk0077@gmail.com>
2023-07-27 03:02:51 +0530

    Commit 19th

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -11,7 +11,7 @@ import { Helmet } from "react-helmet";
 
 const testimonials = [
   {
-    name: "Harsh Kumar",
+    name: "",
     role: "Frontend Web Developer",
     content:
       "A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.",
@@ -72,7 +72,7 @@ function TestimonialCard(props) {
           {content}
         </chakra.p>
         <chakra.p
-          fontFamily={"Work Sans"}
+          fontFamily={"Inter"}
           color={useColorModeValue("gray.700", "gray.50")}
           fontWeight={"bold"}
           fontSize={14}
