2b4e2724f139a678536ab99d33e6cabfcd804f22
harshkumar0077 <harsh.kk0077@gmail.com>
2023-07-27 03:03:08 +0530

    Name updated

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -11,7 +11,7 @@ import { Helmet } from "react-helmet";
 
 const testimonials = [
   {
-    name: "",
+    name: "Harsh Kumar",
     role: "Frontend Web Developer",
     content:
       "A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.",
